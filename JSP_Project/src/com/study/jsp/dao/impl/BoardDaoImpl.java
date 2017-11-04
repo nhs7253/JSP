f27d6d3976637ab8d30efa8e68566f218d6e1440
hs <hs@hs-PC>
2017-11-04 13:33:44 +0900

    JSP_Project
    DaoImpl, DaoImplTest 패키지 추가

---
+++ JSP_Project/src/com/study/jsp/dao/impl/BoardDaoImpl.java
@@ -0,0 +1,25 @@
+package com.study.jsp.dao.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.study.jsp.dao.BoardDao;
+import com.study.jsp.vo.Board;
+
+@Repository
+public class BoardDaoImpl implements BoardDao{
+
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	private String namespace="com.study.jsp.config.mybatis.mapper.BoardMapper.";
+	
+	@Override
+	public List<Board> selectBoardAll() {
+		return session.selectList(namespace+"selectBoardAll");
+	}
+
+}
